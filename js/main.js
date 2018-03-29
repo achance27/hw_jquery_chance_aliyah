8229a1eff63edba69cfb5eb30730e49456e4a220
aliyahchance <aliyahachance@gmail.com>
2018-03-29 19:15:10 -0400

    Remove unnecessary

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // No. 11
 $(document).ready(function () {
     var headings = $('h2');
-    $(headings).last().text('I\'m Aliyah Chance, and I\'m learning jQuery!');
+    (headings).last().text('I\'m Aliyah Chance, and I\'m learning jQuery!');
 });
